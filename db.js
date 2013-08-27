6d04bbe643585927be6e9be2a916b1b3f1fac342
Anton Skshidlevsky <meefik@gmail.com>
2013-08-27 17:56:45 +0400

    Reconnect to mysql after connection lost.

--- db.js
+++ db.js
@@ -1,12 +1,27 @@
 var mysql = require('mysql');
 var db_conf = require('./dbconf.json');
-var conn = mysql.createConnection(db_conf);
 
+var conn = mysql.createConnection(db_conf);
 conn.connect(function(err) {
     if (err) {
-        console.log("Error connection to database.");
+        console.error("Error connection to database.");
     }
 });
+
+handleDisconnect(conn);
+
+function handleDisconnect(client) {
+    client.on('error', function (error) {
+        if (!error.fatal) return;
+        if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw error;
+
+        console.error('> Re-connecting lost MySQL connection: ' + error.stack);
+
+        conn = mysql.createConnection(client.config);
+        handleDisconnect(conn);
+        conn.connect();
+    });
+};
 
 exports.db = function() {
     return conn;
